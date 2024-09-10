af5a2ff55f587d9fb7e79add5dfb5de3908b32e1
adruijter <adruijter@gmail.com>
2024-09-10 11:45:25 +0200

    Laatste commit eerste twee lesuren

--- app/lesweek1.php
+++ app/lesweek1.php
@@ -10,27 +10,50 @@
 <body>
     <h1>Tekst op scherm zetten met PHP</h1>
 
+    <!-- Dit is HTML commentaar-->
+
     <?php
+       // Dit is een variabele voor de voornaam 
        $voornaam = "Arjan";
        
+       # Dit is een variabele voor de tussenvoegsel
        $tussenvoegsel = "de";
        
+       /* Dit is
+          commentaar op
+          meerdere regels*/
        $achternaam = "Ruijter";
 
+       /**
+        * Dit is een docblock comment wat veel
+        * gebruikt wordt bij functies.
+        * Dit zullen we later tegenkomen
+        */
        $straatnaam = "Julianastraat";
 
        $huisnummer = "6HS";
 
        $woonplaats = "Amsterdam";
 
+       $naamVader = 'Frans';
+
        echo "<div>";
              
        echo "<p>Mijn naam is $voornaam $tussenvoegsel $achternaam</p>"; 
 
-       echo '<p>Mijn naam is ' . $voornaam . ' ' .$tussenvoegsel . ' ' . $achternaam . '</p>';
+       /* Hieronder staat een voorbeeld van hoe je een string in stukjes kunt knippen en weer
+          aan elkaar kunt plakken*/
+
+       // echo '<p>Mijn naam is ' . $voornaam . ' ' .$tussenvoegsel . ' ' . $achternaam . '</p>';
        
        echo "<p>Ik woon in de $straatnaam op huisnummer $huisnummer te $woonplaats </p>";
 
+       echo "</div>";
+
+
+       
+       echo "<div>";
+       echo "De naam van mijn vader is: $naamVader";
        echo "</div>";
     ?>
 
