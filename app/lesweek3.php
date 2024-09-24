e132f0bde4667389f7a9e6c3b16f7f55cc4916c8
adruijter <adruijter@gmail.com>
2024-09-24 11:06:26 +0200

    nieuwe commit

--- app/lesweek3.php
+++ app/lesweek3.php
@@ -29,10 +29,31 @@
 
         echo "<p>De som van $getal1 + $getal2 = $som</p>";
 
-        function berekenSom() 
+        function naamOpScherm($voornaam, $tussenvoegsel = NULL, $achternaam = NULL) 
         {
-            
+            echo "<p>Dit is mijn eerste functie en mijn naam is $voornaam $tussenvoegsel $achternaam</p>";
         }
+
+        naamOpScherm('Arjan', 'de', 'Ruijter');
+        naamOpScherm('Bert', 'de', 'Vries');
+        naamOpScherm('Ronald');
+
+        /**
+         * Opdracht:
+         * Zet ook nog het tussenvoegsel bij de functie zodat de volledige naam
+         * met tussenvoegsel op het scherm komt. Verander ook de functienaam 
+         * naar een beter passende dan berekenSom()
+         */
+
+         /**
+          * Maak een functie berekenSom() die twee getallen kan optellen. Het is verplicht om
+          * Twee getallen mee te geven aan de functie. Roep de functie 3 maal aan
+          * met de waarden (3, 4) (9, 7) en (4, 2). De output is:
+          * De som van 3 + 4 = 7
+          * De som van 9 + 7 = 16
+          * De som van 4 + 2 = 6
+          */
+       
 
     ?>
     
