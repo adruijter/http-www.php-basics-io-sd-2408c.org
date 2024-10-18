ae79a06d0e99e8f99a468afadf00b709a483f5d5
adruijter <adruijter@gmail.com>
2024-10-18 14:26:16 +0200

    vraag 4

--- app/proeftoets.php
+++ app/proeftoets.php
@@ -10,5 +10,91 @@
 <body>
     <h3>Proeftoets</h3>
     
+    <h5>Vraag 2</h5>
+    <hr>
+
+    <?php
+
+    $noot = array(
+        'Cashewnoten',
+        'Paranoten',  
+        'Hazelnoten',
+        'Amandelen',
+        'Walnoten'
+    );
+
+    $index = 1;
+    foreach ($noot as $nootje) {
+        echo "<p>$index. $nootje</p>";
+        $index++;
+    }
+
+    echo "<hr>";
+
+    echo "<ol>";
+    foreach ($noot as $nootje) {
+        echo "<li>$nootje</li>";
+        $index++;
+    }
+    echo "</ol>";
+
+    echo "<hr>";
+
+    foreach ($noot as $index => $nootje) {
+        echo "<p>" . $index + 1 . ". $nootje</p>";
+    }
+    ?>
+
+    <h5>Vraag 3</h5>
+    <hr>
+
+    <?php
+    $auto = array(
+        'Mercedes' => 120000,
+        'Kia' => 12000,
+        'BMW' => 230000,
+        'Audi' => 88000
+    );
+
+    $totaalBedrag = 0;
+    foreach ($auto as $car => $price) {
+        echo "<p>De $car kost $price</p>";
+        $totaalBedrag += $price;
+    }
+    echo "Het totaalbedrag voor het wagenpark is: $totaalBedrag &euro;";
+    ?>
+
+    <h5>Vraag 4</h5>
+    <hr>
+
+    <?php
+    function rekenen($getal1, $getal2, $bewerking)
+    {
+        switch ($bewerking) {
+            case '+':
+                $som = $getal1 + $getal2;
+                return "<p>De som van $getal1 + $getal2 = $som</p>";
+                break;
+            case ':':
+                $deling = $getal1 / $getal2;
+                return "<p>De deling van $getal1 : $getal2 = $deling</p>";
+                break;
+            case 'x':
+                $product = $getal1 * $getal2;
+                return "<p>Het product van $getal1 x $getal2 = $product</p>";
+                break;
+            case 'macht':
+                $macht = $getal1 ** $getal2;
+                return "<p>$getal1 tot de macht $getal2 = $macht</p>";
+                break;
+            default:
+                return "<p>Uw gekozen bewerking is niet bekend.</p>";
+        }        
+    }
+
+    echo rekenen(5, 3, '+');
+    echo rekenen(10, 5, ':');
+
+    ?>
 </body>
 </html>
